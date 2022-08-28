09076e7c481671b54475e0461aaf2f92cd39c02c
ChickenBenny <zxc123benny14159@gmail.com>
2022-08-28 22:06:05 +0800

    create table sql

---
+++ database/sql/create_table.sql
@@ -0,0 +1,17 @@
+-- create a table to store the channelID and channelTitle
+CREATE TABLE IF NOT EXISTS "channel" (
+    channel_id TEXT NOT NULL PRIMARY KEY,
+    channel_title TEXT NOT NULL
+); 
+
+ -- create a table to store vide id and channelID
+CREATE TABLE IF NOT EXISTS video (
+    channel_id TEXT NOT NULL REFERENCEs channel(channel_id),
+    video_id TEXT NOT NULL PRIMARY KEY
+);  
+
+  -- create a table to store the video which had been watched
+CREATE TABLE IF NOT EXISTS history (
+    dt DATE NOT NULL, 
+    video_id TEXT NOT NULL REFERENCEs video(video_id) 
+);
