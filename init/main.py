91ebbaf5cab37742cf921b35841afc9555228823
ChickenBenny <zxc123benny14159@gmail.com>
2022-08-29 23:15:03 +0800

    init db

--- init/main.py
+++ init/main.py
@@ -2,14 +2,6 @@ import psycopg2
 import requests
 import json
 
-connection = psycopg2.connect(
-                                database = "airflow",
-                                user = "airflow",
-                                password = "airflow",
-                                host = "database",
-                                port = '5432'
-                            )
-
 
 def get_api():
     with open('./credentials/youtube_api.json', 'r') as fp:
@@ -23,7 +15,7 @@ def get_information(video: str, api_key: str):
     
     channelId = (response_video['items'][0]).get('snippet').get('channelId')
     channelTitle = (response_video['items'][0]).get('snippet').get('channelTitle')
-    print(channelId, '\n', channelTitle)
+    print(channelId, ' ', channelTitle)
 
     url_channel = f'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id={channelId}&key={api_key}'
     response_channel = requests.get(url_channel)
@@ -39,20 +31,49 @@ def get_information(video: str, api_key: str):
     videoId = []
     for info in response_playlist['items']:
         video_id = info['contentDetails'].get('videoId')
-        videoId.append(video_id)
+        videoId.append((channelId, video_id))
     return channelId, channelTitle, videoId
 
+def insert_channel(channelId, channelTitle):
+    connection = psycopg2.connect(
+                                database = "airflow",
+                                user = "airflow",
+                                password = "airflow",
+                                host = "database",
+                                port = '5432'
+                            )
+    cursor = connection.cursor()
+    query = f'''INSERT INTO "channel"(channel_id, channel_title) VALUES('{channelId}', '{channelTitle}')'''
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
 
+def insert_video(data):
+    connection = psycopg2.connect(
+                                database = "airflow",
+                                user = "airflow",
+                                password = "airflow",
+                                host = "database",
+                                port = '5432'
+                            )
+    cursor = connection.cursor()
+    query = f'''INSERT INTO video(channel_id, video_id) VALUES(%s, %s)'''
+    cursor.executemany(query, data)
+    connection.commit()
+    cursor.close()
+  
 if __name__ == '__main__':
-    video_data = ['vSmR5kOKPdY']
+    data = ['AOIzIh6-q9A&t=547s', 'XV2H9CuQUm8', 'uLnmTXxpK0Q'] # 可以塞你想要的頻道
     token = get_api()
-    # for video in video_data:
-    #     channelId, channelTitle, videoId = get_information(video, token)
-    #     cur = connection.cursor()
-    #     # insert channel information
-    #     query = f'''INSERT INTO "channel"(channel_id, channel_title) VALUES('{channelId}', '{channelTitle}')'''
-    #     cur.execute(query)
-    #     cur.close()
-    cur = connection.cursor()
-    query = f'''INSERT INTO "channel"(channel_id, channel_title) VALUES('a', 'b');'''
-    cur.close()
+    for video in data:
+        channelId, channelTitle, videoId = get_information(video, token)
+        try:
+            insert_channel(channelId, channelTitle)
+            print(f"{channelTitle} insert complete!")
+        except:
+            print(f"{channelTitle} already in db!")
+        try:
+            insert_video(videoId)
+            print(f"{channelTitle} video insert complete!")
+        except:
+            print(f"{channelTitle} video already in db!")
